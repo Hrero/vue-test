bc145db0f2666d0aa57e47ba0a68dda4ed144888
hanxiaoyu <hanxiaoyu@e-eduspace.com>
2016-12-22 16:13:26 +0800

    package.json

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
 import Vue from 'vue'
 import vueRouter from "vue-router"
-
 import App from './components/App'
-
 import router from "./routes/pageConfig"
 
 
 Vue.use(vueRouter);
 
-//wangeditor
+//第三方
 import "wangeditor/dist/css/wangEditor.min.css"
 import "wangeditor"
-//ztree
 import "ztree/css/zTreeStyle/zTreeStyle.css"
 import 'ztree'
-//重置样式
-import "./css/reset.scss";
-//semantic-ui
 import "./public/semantic/semantic.min.css"
 import "./public/semantic/semantic.min"
+import "./mathjax"
 
 //自定义指令
 import "./order"
-import "./mathjax"
-
-/*import latex from "latextomathml"
-console.log(latex.LaTeXtoMathML("(a+b)^{\\frac mn} = \\sqrt[n]{(a+b)^m}"));*/
-
-
-/*$.get("/qiniu",function(data){
-    console.log(data);
-});*/
 
 new Vue({
     el: '#app',
